19b368e6b18cebba4c59e0b6040738d7a30db613
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-03-11 23:22:38 +0100

    add movement to player class

--- src/core/handler/event.py
+++ src/core/handler/event.py
@@ -3,39 +3,39 @@ from src import data
 from src.game.input import Input
 
 def event_handler(event):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for _event in event:
+        if _event.type == pygame.QUIT:
             data.game_running = False
             
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+        if _event.type == pygame.KEYDOWN:
+            if _event.key == pygame.K_ESCAPE:
                 data.game_running = False
             
-            if event.key == pygame.K_j:
+            if _event.key == pygame.K_j:
                 data.game_input.player_left(0)
-            if event.key == pygame.K_l:
+            if _event.key == pygame.K_l:
                 data.game_input.player_right(0)
-            if event.key == pygame.K_i:
+            if _event.key == pygame.K_i:
                 data.game_input.player_jump(0)
             
-            if event.key == pygame.K_q:
+            if _event.key == pygame.K_q:
                 data.game_input.player_left(1)
-            if event.key == pygame.K_d:
+            if _event.key == pygame.K_d:
                 data.game_input.player_right(1)
-            if event.key == pygame.K_z:
+            if _event.key == pygame.K_z:
                 data.game_input.player_jump(1)
             
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_j:
+        if _event.type == pygame.KEYUP:
+            if _event.key == pygame.K_j:
                 data.game_input.player_cancel_left(0)
-            if event.key == pygame.K_l:
+            if _event.key == pygame.K_l:
                 data.game_input.player_cancel_right(0)
-            if event.key == pygame.K_i:
+            if _event.key == pygame.K_i:
                 data.game_input.player_cancel_jump(0)
             
-            if event.key == pygame.K_q:
+            if _event.key == pygame.K_q:
                 data.game_input.player_cancel_left(1)
-            if event.key == pygame.K_d:
+            if _event.key == pygame.K_d:
                 data.game_input.player_cancel_right(1)
-            if event.key == pygame.K_z:
+            if _event.key == pygame.K_z:
                 data.game_input.player_cancel_jump(1)

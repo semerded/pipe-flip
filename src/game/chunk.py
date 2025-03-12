c2f02684304cd0b72e558812bebea2f03ebea937
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2025-03-12 22:57:30 +0100

    update rendering & player movement with delta

--- src/game/chunk.py
+++ src/game/chunk.py
@@ -12,17 +12,24 @@ class Chunk:
         self.rect = rect
         self.TEXTURE_PATH_BASE = "assets/textures/chunks/"
         self.textures = []
+        self.stage_update = True
+        
         
         
     def _load_textures(self):
         pass
         # pygame.image.load(self.TEXTURE_PATH_BASE + "0" + self.player.color_blind_type.value + ".png")
+    
+    def cycle(self, color):
+        if self.player.cycle():
+            pygame.draw.rect(data.window, color, self.rect.pack())
+            self.player.draw()
+            
         
     def draw(self, color):
         pygame.draw.rect(data.window, color, self.rect.pack())
-        
-        self.player.move()
         self.player.draw()
         
-    def update(self):
+    def update(self):        
+        self.player.draw()
         pygame.display.update(self.rect)
